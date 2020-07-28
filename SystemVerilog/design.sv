0149fa08c94491b93e47225ca68649b4cddd5076
Parker Lloyd <parkerlloyd44@gmail.com>
2020-07-28 15:10:02 -0700

    Update design.sv

--- SystemVerilog/design.sv
+++ SystemVerilog/design.sv
@@ -3,16 +3,15 @@
  *
  *	Description:	This program accepts a 4-bit command from an MCU to read/write
  *					values that are stored in two 8-bit registers.
- *					Possible commands are:
- *              	Valid commands are:
+ *          Valid commands are:
  *					 - "xxx0"	: points to register A	*WILL NOT BE RUN BY ITSELF*
  *					 - "xxx1"	: points to register B	*WILL NOT BE RUN BY ITSELF*
- *                	 - "000x"	: sets the value of the pointed register to zero
- *                	 - "001x"	: increments the value of the pointed register
- *                	 - "010x"	: decrements the value of the pointed register
+ *           - "000x"	: sets the value of the pointed register to zero
+ *           - "001x"	: increments the value of the pointed register
+ *           - "010x"	: decrements the value of the pointed register
  *					 - "011x"	: inputs the next value into the pointer register
- *                	 - "100x	: decrements the value of the pointed register and repeats the previous command
- *                	 - "101x"	: outputs the value of pointed register on the CPLD I/O
+ *           - "100x	: decrements the value of the pointed register and repeats the previous command
+ *           - "101x"	: outputs the value of pointed register on the CPLD I/O
  */
 module two_byte_memory(clk, en, pc_in, data_out, int_wait, data_a, data_b);
   
@@ -20,24 +19,24 @@ module two_byte_memory(clk, en, pc_in, data_out, int_wait, data_a, data_b);
   parameter MEM_WIDTH = 8;		// length of the data registers
   parameter	ADDR_WIDTH = 1;		// 1-bit: 0 -> A, 1 -> B
   
-  input 						clk;		// clock input
-  input							en;			// enable pulse triggered by new command/input
-  input	 	  [INPUT_WIDTH-1:0]	pc_in; 		// the 4-bit pc command/input value
-  output 	  [MEM_WIDTH-1:0]	data_out;	// the data read from a register
-  output wire					int_wait;	// indicates if CPLD is waiting for integer input value
-  output wire [MEM_WIDTH-1:0]	data_a;		// for viewing contents fo reg(A) w/o output command
-  output wire [MEM_WIDTH-1:0]	data_b;		// for viewing contents fo reg(B) w/o output command
+  input 						            clk;		  // clock input
+  input							            en;			  // enable pulse triggered by new command/input
+  input	 	    [INPUT_WIDTH-1:0]	pc_in; 		// the 4-bit pc command/input value
+  output 	    [MEM_WIDTH-1:0]	  data_out;	// the data read from a register
+  output wire					          int_wait;	// indicates if CPLD is waiting for integer input value
+  output wire [MEM_WIDTH-1:0]	  data_a;		// for viewing contents fo reg(A) w/o output command
+  output wire [MEM_WIDTH-1:0]	  data_b;		// for viewing contents fo reg(B) w/o output command
   
-  reg 	 [MEM_WIDTH-1:0]	data_out;		// too allow data_out to be read by testbench
+  reg 	 [MEM_WIDTH-1:0]	  data_out;		// too allow data_out to be read by testbench
   
-  reg 	 [MEM_WIDTH-1:0]	mem[2**ADDR_WIDTH - 1:0]; // 2-element array of 8-bit wide reg (A and B)
+  reg 	 [MEM_WIDTH-1:0]	  mem[2**ADDR_WIDTH - 1:0]; // 2-element array of 8-bit wide reg (A and B)
   	
   reg 	 [INPUT_WIDTH-1:0] 	input_val;				// input value provided after input command
   reg 	 [INPUT_WIDTH-1:0] 	prev_cmd;				// for saving previous command
   reg 	 [INPUT_WIDTH-1:0] 	curr_cmd;				// for saving current command
-  reg 						curr_addr = 1'b0;		// initialize current address to reg(A)
-  reg 						read_input_val = 1'b0;	// input command not yet provided
-  reg 						repeating = 1'b0;		// repeat/dec command not yet provided
+  reg 						          curr_addr = 1'b0;		// initialize current address to reg(A)
+  reg 						          read_input_val = 1'b0;	// input command not yet provided
+  reg 						          repeating = 1'b0;		// repeat/dec command not yet provided
   
   assign int_wait = read_input_val; // let MCU know whether CPLD is waiting for integer input
   assign data_a = mem[0];			// continously assign current value of reg(A)
